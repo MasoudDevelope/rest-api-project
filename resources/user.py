97eaa4996205a1445b092c3f35e3b131206f5426
masoud <masoudalipoor07@gmail.com>
2023-10-03 11:51:52 -0400

    mail feature added

--- resources/user.py
+++ resources/user.py
@@ -4,21 +4,47 @@ from flask_smorest import Blueprint,abort
 from passlib.hash import pbkdf2_sha256
 from models import UserModel
 from schemas import UserSchema
+from schemas import UserRegisterSchema
 from flask_jwt_extended import create_access_token,jwt_required,get_jwt,create_refresh_token,get_jwt_identity
 from blocklist import BLOCKLIST
+import requests
+import os
+from sqlalchemy import or_
+
 
 blp=Blueprint("User","user",description="operations on user")
+
+domain =os.getenv("MAILGUN_DOMAIN")
+api_key=os.getenv("MAILGUN_API_KEY")
+def send_simple_message(to,subject,body):
+	return requests.post(
+		f"https://api.mailgun.net/v3/{domain}/messages",
+		auth=("api", api_key),
+		data={"from": f"HR <mailgun@{domain}>",
+			"to": [to],
+			"subject": subject,
+			"text": body})
+
+
+
+
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
-    @blp.response(200,UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self,user_data):
-        if UserModel.query.filter(UserModel.username==user_data["username"]).first():
-            abort(500,message=f"A user with username {user_data['username']}  already exists!")
+        if UserModel.query.filter( or_(UserModel.username==user_data["username"] ,
+                                  UserModel.email==user_data["email"]) 
+                                  ).first():
+            abort(500,message=f"A user with the username or email already exists!")
         user = UserModel(username=user_data["username"],
+                         email=user_data["email"],
                          password=pbkdf2_sha256.hash(user_data["password"]))
         db.session.add(user)
         db.session.commit()
+        send_simple_message(to=user.email,
+                            subject="Successfully Signed Up!",
+                            body = f"Hi {user.username}. Welcome to our World!"
+                            )
         return user
 @blp.route("/user/<int:user_id>")
 class UserList(MethodView):
